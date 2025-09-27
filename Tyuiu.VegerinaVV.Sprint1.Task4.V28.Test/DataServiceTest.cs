7d4d467468d94cc53903ca59942ce511398c8ef1
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-09-28 00:04:46 +0500

    Task4

---
+++ Tyuiu.VegerinaVV.Sprint1.Task4.V28.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.VegerinaVV.Sprint1.Task4.V28.Lib;
+
+namespace Tyuiu.VegerinaVV.Sprint1.Task4.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double wait = 0.002;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint1.Task4.V28/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.VegerinaVV.Sprint1.Task4.V28.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Class Math                                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #28                                                             *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x, y;
+
+        Console.WriteLine("Введите значение X: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение Y: ");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("cos(60*pi / 2) / Exp(2 * x + y) = " + ds.Calculate(x, y));
+
+        Console.ReadKey();
+    }
+}
